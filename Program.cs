40a4bc948f8d724be74d00d37772d4be2f7953c3
Bert Sloan <bertsloan@Berts-MacBook-Pro.local>
2018-08-26 15:39:55 -0400

    first

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RTS_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
