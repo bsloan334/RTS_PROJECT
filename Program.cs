68a78e8c99bfc8458c387e4fce5157396594817c
Zach Dillard <zachdillard@users.noreply.github.com>
2018-08-28 11:24:05 -0400

    Added train track struct

--- Program.cs
+++ Program.cs
@@ -1,27 +1,104 @@
-﻿using System;
+using System;
 using System.Threading;
 
 namespace RTS_Project
 {
+    struct TrainTrack
+    {
+        public char[,] planeX;
+        public char[,] planeY;
+        public char[,] planeZ;
+
+        public TrainTrack(ushort row, ushort col)
+        {
+            planeX = new char[row, col];
+            planeY = new char[row, col];
+            planeZ = new char[row, col];
+            ClearTrack(row, col);
+        }
+
+        public void ClearTrack(ushort row, ushort col)
+        {
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    planeX[i, j] = '.';
+                    planeY[i, j] = '.';
+                    planeZ[i, j] = '.';
+                }
+            }
+        }
+
+        public void UpdateTrack()
+        {
+
+        }
+
+        public void UpdateX()
+        {
+
+        }
+
+        public void UpdateY()
+        {
+
+        }
+
+        public void UpdateZ()
+        {
+
+        }
+    }
+
     class Program
     {
         public static System.Object lockA = new System.Object();
         public static System.Object lockB = new System.Object();
+
         public static int[] bufferA = new int[10];
         public static int[] bufferB = new int[10];
+
         public static int iA = 1;
         public static int iB = 1;
 
         static void Main(string[] args)
         {
-            Thread sender = new Thread(Sender);
-            Thread receiver = new Thread(Receiver);
+            Thread sender = new Thread(Producer);
+            Thread receiver = new Thread(Consumer);
 
-            sender.Start();
-            receiver.Start();
+            TrainTrack track = new TrainTrack(8, 7);
+            RunTrains(track);
+
+            //sender.Start();
+            //receiver.Start();
         }
 
-        static void Sender()
+        static void RunTrains(TrainTrack buffer)
+        {
+            buffer.planeX[0, 0] = 'X';
+            buffer.planeY[0, 2] = 'Y';
+            buffer.planeZ[3, 6] = 'Z';
+
+            PrintTrack(buffer.planeX);
+            PrintTrack(buffer.planeY);
+            PrintTrack(buffer.planeZ);
+        }
+
+        static void PrintTrack(char[,] track)
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 7; j++)
+                {
+                    Console.Write(track[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+
+        static void Producer()
         {
             while (iA <= 50 && iB <= 50)
             {
@@ -30,12 +107,12 @@ namespace RTS_Project
                     if ((iA % 10) == 0)
                     {
                         bufferA[9] = iA;
-                        Console.WriteLine("write to bufferA " + bufferA[9]);
+                        Console.WriteLine("write to Buffer A " + bufferA[9]);
                     }
                     else
                     {
                         bufferA[(iA % 10) - 1] = iA;
-                        Console.WriteLine("write to bufferA " + bufferA[(iA % 10) - 1]);
+                        Console.WriteLine("write to Buffer A " + bufferA[(iA % 10) - 1]);
                     }
                     iA++;
                     Thread.Sleep(1000);
@@ -45,12 +122,12 @@ namespace RTS_Project
                     if ((iB % 10) == 0)
                     {
                         bufferB[9] = iB;
-                        Console.WriteLine("write to bufferB " + bufferB[9]);
+                        Console.WriteLine("write to Buffer B " + bufferB[9]);
                     }
                     else
                     {
                         bufferB[(iB % 10) - 1] = iB;
-                        Console.WriteLine("write to bufferB " + bufferB[(iB % 10) - 1]);
+                        Console.WriteLine("write to Buffer B " + bufferB[(iB % 10) - 1]);
                     }
                     iB++;
                     Thread.Sleep(1000);
@@ -58,7 +135,7 @@ namespace RTS_Project
             }
         }
 
-        static void Receiver()
+        static void Consumer()
         {
             while(iA <= 50 && iB <= 50)
             {
@@ -67,7 +144,7 @@ namespace RTS_Project
                     if (IsBufferFull(bufferA) == true)
                     {
                         Console.Write("Buffer A: ");
-                        for (int i = 0; i < 10; i++)
+                        foreach (int i in bufferA)
                         {
                             Console.Write(bufferA[i] + " ");
                             Thread.Sleep(1000);
@@ -81,7 +158,7 @@ namespace RTS_Project
                         if (IsBufferFull(bufferB) == true)
                         {
                             Console.Write("Buffer B: ");
-                            for (int i = 0; i < 10; i++)
+                            foreach (int i in bufferB)
                             {
                                 Console.Write(bufferB[i] + " ");
                                 Thread.Sleep(1000);
@@ -96,7 +173,7 @@ namespace RTS_Project
 
         static bool IsBufferFull(int[] buffer)
         {
-            for (int i = 0; i < 10; i++)
+            foreach (int i in buffer)
             {
                 if (buffer[i] == 0)
                     return false;
@@ -106,7 +183,7 @@ namespace RTS_Project
 
         static void ClearBuffer(int[] buffer)
         {
-            for (int i = 0; i < 10; i++)
+            foreach (int i in buffer) 
             {
                 buffer[i] = 0;
             }
