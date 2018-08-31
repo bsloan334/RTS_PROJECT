bd2663bb5ddecf08f58190eee7ac59e721e20079
Bert Sloan <bertsloan@Berts-MacBook-Pro.local>
2018-08-31 18:43:34 -0400

    fixing stuff

--- Program.cs
+++ Program.cs
@@ -37,20 +37,41 @@ namespace RTS_Project
             UpdateZ(ref currentPositions);
 
             ClearTrack(8, 7);
+            /*
+            The currentpositions array has a value for every slot except the ones we modify in the Update funcitons. I think something is going wrong with the updates
+             */
+
+            // Console.WriteLine(currentPositions[0,0]); 
+            // //Console.WriteLine(currentPositions[0,1]); 
+            // //Console.WriteLine(currentPositions[0,2]); 
+            // Console.WriteLine(currentPositions[1,0]); 
+            // Console.WriteLine(currentPositions[1,1]); 
+            // //Console.WriteLine(currentPositions[1,2]); 
+            // Console.WriteLine(currentPositions[2,0]); 
+            // //Console.WriteLine(currentPositions[2,1]); 
+            // Console.WriteLine(currentPositions[2,2]); 
             planeX[currentPositions[0, 2], currentPositions[0, 1]] = 'X'; 
-            planeY[currentPositions[1, 2], currentPositions[1, 1]] = 'Y'; 
+            planeY[currentPositions[1, 2], currentPositions[1, 1]] = 'Y';
             planeZ[currentPositions[2, 2], currentPositions[2, 1]] = 'Z'; 
         }
 
+        //I updated these functions to correctly swap from char to int and back.
+        //They were not converting to the correct type when I tested.
         public void UpdateX(ref char[,] currentPositions)
         {
             char xChar = currentPositions[0, 2];
-            int xInt = xChar;
+            Console.WriteLine(xChar);
+            int xInt = int.Parse(xChar.ToString());
             xInt = (xInt + 1) % 8;
+            Console.WriteLine(xInt);
+            /*
+            Everything checks out fine up to this point. The next line causes the index out of bounds exception. I do not know why yet.
+             */
             currentPositions[0, 2] = Convert.ToChar(xInt);
+            Console.WriteLine(currentPositions[0,2]);
 
             char yChar = currentPositions[0, 1];
-            int yInt = yChar;
+            int yInt = int.Parse(yChar.ToString());
             yInt = (yInt + 1) % 7;
             currentPositions[0, 1] = Convert.ToChar(yInt);
         }
@@ -58,7 +79,7 @@ namespace RTS_Project
         public void UpdateY(ref char[,] currentPositions)
         {
             char yChar = currentPositions[1, 2];
-            int yInt = yChar;
+            int yInt = int.Parse(yChar.ToString());
             yInt = (yInt + 1) % 8;
             currentPositions[1, 2] = Convert.ToChar(yInt);
         }
@@ -66,7 +87,7 @@ namespace RTS_Project
         public void UpdateZ(ref char[,] currentPositions)
         {
             char xChar = currentPositions[2, 1];
-            int xInt = xChar;
+            int xInt = int.Parse(xChar.ToString());
             xInt = (xInt + 1) % 7;
             currentPositions[2, 1] = Convert.ToChar(xInt);
         }
@@ -79,11 +100,22 @@ namespace RTS_Project
 
         public static TrainTrack track = new TrainTrack(8, 7);
 
-        public static char[,] currentPositions = new char[3, 3];
+        /*
+
+        This array is initialized properly. The reason it goes through without problems when it is not given initial values is because they are all blank without a value to begin with.
+
+        */
+        public static char[,] currentPositions = new char[3, 3] 
+        { 
+            {'X', '0', '0'},
+            {'Y', '0', '2'},
+            {'Z', '3', '6'} }
+        ;
 
         static void Main(string[] args)
         {
 
+            //This will only throw an index out of bounds error if you initialize currentpositions with values
             InitTrains();
 
             for(int i = 0; i < 10; i++) 
